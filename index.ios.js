5d9e94f967197bc01949ad6d016eb7d9c3bfd8d1
Yaroslav Yanko <Iaroslavus@outlook.com>
2016-06-09 15:05:57 +0300

    Add new files to project

---
+++ index.ios.js
@@ -0,0 +1,46 @@
+// Import require lib
+var Moment = require('moment');
+var ReactNative = require('react-native');
+var React = require('react');
+var AppRegistry = ReactNative.AppRegistry;
+var Text = ReactNative.Text;
+var View = ReactNative.View;
+var StyleSheet = ReactNative.StyleSheet;
+var DayItem = require('./src/day-item');
+
+
+
+
+// Create a react component
+var Weekdays = React.createClass({
+  render : function(){
+    return <View style = {styles.container}>
+      {this.days()}
+      
+    </View>
+  },
+  days: function(){
+   var daysItems = [];
+   for( var i = 0; i<7; i++){
+    var day = Moment().add(i, 'days').format('dddd');
+    daysItems.push(
+      <DayItem day = {day} daysUntil={i} />
+      )
+   }
+   return daysItems;
+  }
+});
+
+//Style the react component
+var styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+// Show the react component on the screen
+AppRegistry.registerComponent('weekdays',function(){
+  return Weekdays
+});
